2608d86a06f1beec1988f7e48c052b31e9650e1c
satoshi eric <satoshieric@satoshis-MacBook-Pro.local>
2018-09-15 15:04:55 -0400

    push jchart

---
+++ TestURL 2/src/testurl/testObject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testurl;
+
+/**
+ *
+ * @author jxw5676
+ */
+public class testObject {
+    String time;
+    Object CurrencyValue;
+    
+    private class CurrencyValue {
+        String name;
+        double value;
+    }
+}
